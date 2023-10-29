86f01449356aeb2d482da614e220cec64ededfe0
Imrc0z <68791919+Imrc0z@users.noreply.github.com>
2023-10-29 23:14:19 +0100

    Week 2

--- Opdracht 2.py
+++ Opdracht 2.py
@@ -1,15 +1,3 @@
-while True:
-    tafel = int(input("Welke tafel wilt u printen (0 = stoppen)? "))
-
-    if tafel == 0:
-        break  # Stop het programma als de gebruiker 0 kiest
-
-    aantal_getallen = int(input("Hoeveel getallen wilt u printen? "))
-
-    print(f"De tafel van {tafel}:", end=" ")
-
-    for i in range(1, aantal_getallen + 1):
-        resultaat = tafel * i
-        print(resultaat, end=" ")
-
-    print()  # Print een lege regel om de uitvoer te scheiden
+getal = int(input("Geef een getal!:"))
+if getal != 10:
+    print("OK")
--- Opdracht 3.py
+++ Opdracht 3.py
@@ -1,13 +1,10 @@
-getallen = [3, 7, 0, 15]
+# Opdracht 3
 
+# Vraag de gebruiker om zijn/haar naam
+naam = input("Wat is jouw naam? ")
 
-print("Vermenigvuldigen met 2:")
-for getal in getallen:
-    resultaat = getal * 2
-    print(resultaat, end=" ")
-
-
-print("\Delen door 3 en afronden:")
-for getal in getallen:
-    resultaat = round(getal / 3, 1)
-    print(resultaat, end=" ")
+# Controleer of de naam overeenkomt met "Willem Alexander" of "Maxima"
+if naam == "Willem Alexander" or naam == "Maxima":
+    print("Welkom uwe Hoogheid")
+else:
+    print("Welkom, " + naam)
